e178bedd453292939c895087df32363f5e66f15f
nishimurasyota <syota4723@gmail.com>
2022-03-25 19:43:49 +0900

    :adhesive_bandage: controllerを使用して値を渡して表示する方法について

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -17,77 +17,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
-
-function tag($tag, $txt)
-{
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
 class HelloController extends Controller
 {
 
-    public function index(Request $request, Response $response)
+    public function index()
     {
-        $html = <<<EOF
-
-        <html>
-            <head>
-                <title>Edit</title>
-            </head>
-            <body>
-                <h1>Hello</h1>
-                <h3>Response</h3>
-                <pre>{$response}</pre>
-                <h3>Request</h3>
-                <pre>{$request}</pre>
-            </body>
-        </html>
-        EOF;
-
-        $response->setContent($html);
-        return $response;
+        $data = ["msg" => "コントローラーから渡されたメッセージです"];
+        return view("hello.index", $data);
     }
-    /**
-     * シングルアクションコントローラー
-     *
-     * @return void
-     */
-    // public function __invoke()
-    // {
-    //     return <<<EOF
-
-    // <html>
-    //     <head>
-    //         <title>Edit</title>
-    //     </head>
-    //     <body>
-    //         <h1>Single Action</h1>
-    //         <p>シングルアクションのページです</p>
-    //     </body>
-    // </html>
-    // EOF;
-    // }
-
-    //
-
-    // public function index()
-    // {
-    //     global $head, $style, $body, $end;
-
-    //     $html = $head . tag("title", "Hello/index") . $style . $body
-    //         . tag("h1", "index") . tag("p", "this is Index page")
-    //         . '<a href="/hello/other">go to other page</a>'
-    //         . $end;
-
-    //     return $html;
-    // }
-
-    // public function other()
-    // {
-    //     global $head, $style, $body, $end;
-
-    //     $html = $head . tag("title", "Hello/Other") . $style . $body
-    //         . tag("h1", "Other") . tag("p", "this is other page")
-    //         . $end;
-    //     return $html;
-    // }
 }
