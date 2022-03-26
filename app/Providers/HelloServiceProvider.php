14d296039d7463ce3d9e74d204c2e2d388dfeca0
nishimurasyota <syota4723@gmail.com>
2022-03-26 17:29:43 +0900

    :adhesive_bandage: ビューコンポーザクラスを作って記述してみる

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -25,9 +25,8 @@ class HelloServiceProvider extends ServiceProvider
     public function boot()
     {
         View::Composer(
-            "hello.index", function($view){
-                $view->with("view_message", "composer message");
-            }
+            "hello.index",
+            "App\Http\Composers\HelloComposer"
         );
     }
 }
