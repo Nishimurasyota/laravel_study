7c839561a11c0d36224704ee5d1069c8ee037be6
nishimurasyota <syota4723@gmail.com>
2022-03-28 16:48:00 +0900

    :adhesive_bandage: ミドルウェアのグループ登録と実装

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\HelloMiddleware::class,
     ];
 
     /**
@@ -45,6 +44,9 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        "hello" => [
+            \App\Http\Middleware\HelloMiddleware::class,
+        ]
     ];
 
     /**
