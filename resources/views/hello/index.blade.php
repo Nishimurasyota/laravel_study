ccebfe3eab34ebffae31bec3b7730be39e0fa993
nishimurasyota <syota4723@gmail.com>
2022-03-30 20:03:16 +0900

    :adhesive_bandage: 項目ごとにエラーメッセージを表示する

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -12,25 +12,39 @@
 <p>{{$msg}}</p>
 @if(count($errors) > 0)
 <div>
-    <ul>
-        @foreach($errors->all() as $error)
-        <li>{{$error}}</li>
-        @endforeach
-    </ul>
+    <p>入力に誤りがあります</p>
 </div>
 @endif
 <form action="/hello" method="post">
 
     <table>
         @csrf
+        @if($errors->has('name'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('name')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>name: </th>
             <td><input type="text" name="name" value="{{old('name')}}"></td>
         </tr>
+        @if($errors->has('mail'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('mail')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>mail: </th>
             <td><input type="text" name="mail" value="{{old('mail')}}"></td>
         </tr>
+        @if($errors->has('age'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('age')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>age: </th>
             <td><input type="text" name="age" value="{{old('age')}}"></td>
