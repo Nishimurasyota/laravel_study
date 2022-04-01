b305a90be46f3510f3bd5891f01b7c6ab64f09cc
nishimurasyota <syota4723@gmail.com>
2022-04-01 13:42:58 +0900

    :adhesive_bandage: getDataメソッドを定義、実装
    Personモデルにid,name,ageをまとめて出力するメソッド、getDataを定義

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -10,16 +10,9 @@ Index
 @section("content")
 
 <table>
-    <tr>
-        <th>Name</th>
-        <th>Mail</th>
-        <th>Age</th>
-    </tr>
     @foreach($items as $item)
     <tr>
-        <td>{{$item->name}}</td>
-        <td>{{$item->mail}}</td>
-        <td>{{$item->age}}</td>
+        <td>{{$item->getData()}}</td>
     </tr>
     @endforeach
 </table>
