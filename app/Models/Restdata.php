2bf25c586c67b169359a00515047b738def18fa4
nishimurasyota <syota4723@gmail.com>
2022-04-04 11:26:37 +0900

    :adhesive_bandage: Restdataモデルの内容を修正

--- app/Models/Restdata.php
+++ app/Models/Restdata.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restdata extends Model
 {
-    use HasFactory;
+    protected $table = "restdata";
+    protected $guarded = array("id");
+
+    public static $rules = array(
+        "message" => "required",
+        "url" => "required",
+    );
+
+    public function getData(){
+        return $this->id . ":" . $this->message . "(". $this->url .")";
+    }
 }
