ff34cf1ce5cb5ebe79b8ad1d91ed4155932f5135
nishimurasyota <syota4723@gmail.com>
2022-04-04 11:58:49 +0900

    :adhesive_bandage: seedの実行

--- database/seeders/RestdataSeeder.php
+++ database/seeders/RestdataSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Restdata;
 
 class RestdataSeeder extends Seeder
 {
@@ -14,6 +15,25 @@ class RestdataSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param = [
+            "message" => "Google Japan",
+            "url" => "http://www.google.com/",
+        ];
+        $restdata = new Restdata;
+        $restdata->fill($param)->save();
+
+        $param = [
+            "message" => "Yahoo Japan",
+            "url" => "http://www.yahoo.co.jp/",
+        ];
+        $restdata = new Restdata;
+        $restdata->fill($param)->save();
+
+        $param = [
+            "message" => "MSN Japan",
+            "url" => "http://www.msn.com/ja-jp",
+        ];
+        $restdata = new Restdata;
+        $restdata->fill($param)->save();
     }
 }
